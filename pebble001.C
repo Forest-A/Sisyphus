d068adeec7bbec5cc231448d7d1377ae542755ef
Forest-A <holdencamus666@gmail.com>
2024-09-10 16:19:15 +0100

    commit

--- pebble001.C
+++ pebble001.C
@@ -6,21 +6,25 @@
 #include <TMinuit.h>
 #include <TRandom3.h>
 #include <TStyle.h>
-#include <TList.h>
 #include <TLegend.h>
+#include <TSystem.h>
 #include "/home/motoko/TKI_Comparison/style/style.h"
 
-// Global pointer to the random number generator, histogram, and file
-TRandom3 *gRand = new TRandom3(123);
+// Global pointers to the random number generator and histogram
+TRandom3 gRand(123);
 TH1D *gHist = nullptr;
-TFile *outputFile = nullptr;
-TList *plotList = nullptr;
+TCanvas *gCanvas = nullptr;  // Global canvas pointer
 
 // Generate Gaussian values and fill histogram
 void GenerateGaussianValues(TH1D *hist, const double mean, const double stdDev) {
   
+    if (!hist) {
+      std::cerr << "Error: Histogram not properly initialised!" << std::endl;
+      return;
+    }
+    
     for (int ii = 0; ii < 1e5; ii++) {
-        hist->Fill(gRand->Gaus(mean, stdDev));
+        hist->Fill(gRand.Gaus(mean, stdDev));
     }
 }
 
@@ -36,20 +40,20 @@ void fcn(int &npar, double *gin, double &ff, double *par, int iflag) {
     double chi2 = 0.0;
 
     for (int ii = 1; ii <= nBins; ii++) {
-        double x = gHist->GetBinCenter(ii);
-        double data = gHist->GetBinContent(ii);
-        double fit = par[0] * TMath::Gaus(x, par[1], par[2]);
+        const double xx = gHist->GetBinCenter(ii);
+        const double data = gHist->GetBinContent(ii);
+        double fit = par[0] * TMath::Gaus(xx, par[1], par[2]);
 
         if (data > 0) {
-            chi2 += TMath::Power((fit - data), 2) / data;
+	  chi2 += TMath::Power((fit - data), 2) / TMath::Power(gHist->GetBinError(ii), 2);
         }
     }
     ff = chi2;
 }
 
 // Perform the fit using Minuit
-double SingleFit(TF1* fitFunc, double* params, double* errors, double& chi2) {
-
+int SingleFit(TF1* fitFunc, double* params, double* errors, double& chi2) {
+  
     TMinuit Minuit(3);
     Minuit.SetFCN(fcn);
 
@@ -77,32 +81,17 @@ double SingleFit(TF1* fitFunc, double* params, double* errors, double& chi2) {
 }
 
 // Plot the Gaussian fit at each iteration and save it
-void PlotGaussianFit(const int iteration, TF1 *fitFunc, const double *params, const double *errors, TList *list) {
-  
-    TCanvas *c1 = new TCanvas(Form("c1_iter_%d", iteration), "Gaussian Fit", 800, 600);
-    gStyle->SetOptStat(0);
+void PlotGaussianFit(const int iteration, TF1 *fitFunc, const double *params, const double *errors, TFile *outputFile) {
+    // Use the global canvas
+    if (!gCanvas) {
+      gCanvas = new TCanvas(Form("c1_iter_%d", iteration), "Gaussian Fit", 800, 600);
+    }
+    else {
+        gCanvas->cd();  // Make sure to use the existing canvas
+    }
 
-    // Canvas Setup
-    style::SetGlobalStyle();
-    style::fgkYTitleOffset=1.6;
-    style::IniColorCB();
+    gCanvas->Clear();  // Clear the canvas for new plot
 
-    Double_t currentLeft = 0.17; 
-    Double_t currentTop = 0.06; 
-    Double_t currentRight = 0.035;
-    Double_t currentBottom = 0.14;
-
-    style::PadSetup(c1);
-
-    gPad->SetLeftMargin(currentLeft);
-    gPad->SetRightMargin(currentRight);
-    gPad->SetTopMargin(currentTop);
-    gPad->SetBottomMargin(currentBottom);
-
-    // Title
-    gStyle->SetOptTitle(1);
-    gStyle->SetTitleX(0.6);
-    gStyle->SetTitleW(0.8);
     gHist->SetTitle("Gaussian Fitting");
 
     // Draw histogram and fit function
@@ -134,35 +123,27 @@ void PlotGaussianFit(const int iteration, TF1 *fitFunc, const double *params, co
     gHist->GetXaxis()->SetTitle("x-axis");
     gHist->GetYaxis()->SetTitle("y-axis");
 
-    // Save the list to the ROOT file
-    list->Add(c1);
+    // Print out the plots
+    gCanvas->Print(Form("GaussianFit_Iteration_%d.png", iteration));
 
-    // Save the list to the ROOT file
-    list->Write(Form("Plot_Iteration_%d", iteration), TObject::kOverwrite);
+    // Save the canvas to the ROOT file
+    outputFile->cd();
+    gCanvas->Write(Form("GaussianFit_Iteration_%d", iteration), TObject::kOverwrite);
     
     delete lg;  // Deleting legend
 }
 
 // Perform iterative fitting with convergence check
-void IterativeFit(TF1* fitFunc, double* params, double* errors, const int maxIterations, const double convergenceThreshold) {
-  
+void IterativeFit(TF1* fitFunc, double* params, double* errors, const int maxIterations, const double convergenceThreshold, TFile *outputFile) {
     double previousChi2 = 1e10;
     double currentChi2 = 0.0;
-
-    // Create a ROOT file to store results
-    outputFile = new TFile("GaussianFits.root", "RECREATE");
-    if (!outputFile || outputFile->IsZombie()) {
-        std::cerr << "Error: Cannot create ROOT file." << std::endl;
-        return;
-    }
-    plotList = new TList();
 
     for (int ii = 0; ii < maxIterations; ii++) {
         // Perform a single fit
         double status = SingleFit(fitFunc, params, errors, currentChi2);
 
         // Plot and save the results for this iteration
-        PlotGaussianFit(ii + 1, fitFunc, params, errors, plotList);
+        PlotGaussianFit(ii + 1, fitFunc, params, errors, outputFile);
 
         // Check for convergence
         if (status != 0) {
@@ -176,14 +157,19 @@ void IterativeFit(TF1* fitFunc, double* params, double* errors, const int maxIte
             break;
         }
 
-        previousChi2 = currentChi2;	
+        previousChi2 = currentChi2;
     }
-    
-    delete plotList;
-    delete outputFile;
 }
 
 int main() {
+
+    // Redirect ROOT output to the log file
+    Int_t status = gSystem->RedirectOutput("see.log", "w"); // "w" to overwrite the file
+    if (status != 0) {
+        std::cerr << "Error: Unable to redirect output to file." << std::endl;
+        return 1;
+    }
+    
     // Create histogram
     gHist = new TH1D("hist", "Generated Gaussian Data", 100, -10, 10);
 
@@ -198,13 +184,53 @@ int main() {
     };
     double errors[3];
 
+    // Create a ROOT file to store results
+    TFile *ff = new TFile("GaussianFits.root", "RECREATE");
+    if (!ff || ff->IsZombie()) {
+        std::cerr << "Error: Cannot create ROOT file." << std::endl;
+        return 1;
+    }
+
+    // Canvas Setup
+    gStyle->SetOptStat(0);
+    style::SetGlobalStyle();
+    style::fgkYTitleOffset = 1.6;
+    style::IniColorCB();
+
+    Double_t currentLeft = 0.17; 
+    Double_t currentTop = 0.06; 
+    Double_t currentRight = 0.035;
+    Double_t currentBottom = 0.14;
+
+    // Ensure the global canvas is created with appropriate margins
+    gCanvas = new TCanvas("gCanvas", "Gaussian Fit", 800, 600);
+    gPad->SetLeftMargin(currentLeft);
+    gPad->SetRightMargin(currentRight);
+    gPad->SetTopMargin(currentTop);
+    gPad->SetBottomMargin(currentBottom);
+
+    // Title
+    gStyle->SetOptTitle(1);
+    gStyle->SetTitleX(0.6);
+    gStyle->SetTitleW(0.8);
+
     // Perform the fit
     TF1 *fitFunc = new TF1("fitFunc", "[0] * TMath::Gaus(x,[1],[2])", -1e3, 1e3);
-    IterativeFit(fitFunc, params, errors, 100, 1e-6);
+    
+    IterativeFit(fitFunc, params, errors, 100, 1e-6, ff);
 
+    // Save everything and close the file
+    ff->Write(); 
+    ff->Close();
+
+    // Restore output to the default streams
+    gSystem->RedirectOutput(0);
+    
+    // Cleanup
+    delete ff;
     delete fitFunc;
     delete gHist;
-    delete gRand;
-    
+    delete gCanvas; // Delete global canvas
+
     return 0;
 }
